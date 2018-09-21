277a92183786f10f9f5674f91dfe33e9ca18f4b3
James Wall <jamwall@microsoft.com>
2018-09-21 05:23:55 +0100

    very minor additions, taking this slow

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 # main.py
 # going to try and make a Python Facebook photo downloader
 
-import facebook
+#import facebook
+
+def getTokenFromUser():
+
+    # ask for token from user
+    token = input("Hey can you give me your Facebook user token?")
+
+    # it pays to be polite
+    print ("Thanks!")
+
+    # return token
+    return token
+
+
+getTokenFromUser()
