d3a9d512ca4fa53fbb73e0c9f7509b817e58a3ca
James Wall <jamwall@microsoft.com>
2018-09-22 22:31:39 +0100

    added tentative skeleton structure of program

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 # main.py
-# going to try and make a Python Facebook photo downloader
+# script to batch download Facebook photos from an individual account
 
-#import facebook
+# start LIBRARIES
+import facebook
+# end LIBRARIES
+
+# start METHODS
 
 def getTokenFromUser():
 
     # ask for token from user
-    token = input("Hey can you give me your Facebook user token?")
+    token = str(input("Hey can you give me your Facebook user token?")) # TODO add input error handling
 
     # it pays to be polite
     print ("Thanks!")
@@ -14,5 +18,47 @@ def getTokenFromUser():
     # return token
     return token
 
+def getLoginInformationFromUser():
+    # TODO
+    return [1, 2] # TODO fix 
 
-getTokenFromUser()
+def getInformationFromUser():
+    loginInfo = []
+    loginInfo.append(getTokenFromUser())
+    loginInfo.append(getLoginInformationFromUser()) # TODO list of lists here
+
+    return loginInfo
+
+def establishLoginWithAPI():
+    # TODO
+	todo = 1
+
+def getPhotoDetailsFromUser():
+    # TODO how many photos?
+    # when should the photos be from?
+    # what date range?
+    # who should be in the photos?
+	todo2 = 2
+
+def downloadPhotosToLocation():
+    # TODO use information from user to download photos
+    # optional ask of where to download photos, otherwise download in a default location (like a desktop folder)
+	todo3 = 3
+
+# end METHODS
+
+# 
+#
+#
+#
+# start MAIN
+
+getInformationFromUser()
+
+establishLoginWithAPI()
+
+getPhotoDetailsFromUser()
+
+downloadPhotosToLocation()
+
+# end MAIN
