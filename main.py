78717aa91e5eda4325e4c18384712ede09fd1ded
James Wall <jamwall@microsoft.com>
2018-09-24 00:08:46 +0100

    Successfully got OAuth to work to generate user access token

--- main.py
+++ main.py
@@ -3,46 +3,67 @@
 
 # start LIBRARIES
 import facebook
+import requests
 # end LIBRARIES
 
 # start METHODS
 
+def get_fb_token(app_id, app_secret):
+	url = 'https://graph.facebook.com/oauth/access_token'
+	payload = {
+		'grant_type': 'client_credentials',
+		'client_id': app_id,
+		'client_secret': app_secret
+	}
+	response = requests.post(url, params=payload)
+	
+	# TODO add error handling for invalid response
+	print("json response:")
+	print(response.json())
+	return response.json()['access_token']
+
+# TODO will have to be one ID for non-admins
 def getTokenFromUser():
+	
+	# ask for token from user  # TODO add input error handling
+	app_id = int(input("Hey can you give me your Facebook app ID?"))
+	app_secret = str(input("Great! Can you give me your Facebook app secret?"))
 
-    # ask for token from user
-    token = str(input("Hey can you give me your Facebook user token?")) # TODO add input error handling
+	access_token = get_fb_token(app_id, app_secret)
 
-    # it pays to be polite
-    print ("Thanks!")
+	# it pays to be polite
+	print ("Thanks!")
 
-    # return token
-    return token
+	graph = facebook.GraphAPI(access_token)
+
+	# return token
+	return access_token
 
 def getLoginInformationFromUser():
-    # TODO
-    return [1, 2] # TODO fix 
+	# TODO
+	return [1, 2] # TODO fix 
 
 def getInformationFromUser():
-    loginInfo = []
-    loginInfo.append(getTokenFromUser())
-    loginInfo.append(getLoginInformationFromUser()) # TODO list of lists here
+	loginInfo = []
+	loginInfo.append(getTokenFromUser())
+	loginInfo.append(getLoginInformationFromUser()) # TODO list of lists here
 
-    return loginInfo
+	return loginInfo
 
 def establishLoginWithAPI():
-    # TODO
+	# TODO
 	todo = 1
 
 def getPhotoDetailsFromUser():
-    # TODO how many photos?
-    # when should the photos be from?
-    # what date range?
-    # who should be in the photos?
+	# TODO how many photos?
+	# when should the photos be from?
+	# what date range?
+	# who should be in the photos?
 	todo2 = 2
 
 def downloadPhotosToLocation():
-    # TODO use information from user to download photos
-    # optional ask of where to download photos, otherwise download in a default location (like a desktop folder)
+	# TODO use information from user to download photos
+	# optional ask of where to download photos, otherwise download in a default location (like a desktop folder)
 	todo3 = 3
 
 # end METHODS
